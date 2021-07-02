b38fb4092be64d91c82b1a60e375bbdb77b19960
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-02 23:26:23 +0300

    initial commit

---
+++ app/src/main/java/MoodDialogFragment.java
@@ -0,0 +1,18 @@
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+import com.MooDialogFragment.fragment_app.R;
+
+public class MoodDialogFragment extends DialogFragment implements MoodDialogFragments {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+        return rootView;
+    }
+}
---
+++ app/src/main/java/MoodDialogFragments.java
@@ -0,0 +1,2 @@
+public interface MoodDialogFragments {
+}
